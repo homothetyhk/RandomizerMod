56bb960dfd26e3d711fe6a6bd0f23215a0fe8741
homothetyhk <homothetyhk@gmail.com>
2022-02-23 14:39:04 -0800

    Bump version and add properties to StartDef.

--- RandomizerMod/RandomizerData/StartDef.cs
+++ RandomizerMod/RandomizerData/StartDef.cs
@@ -7,19 +7,44 @@ namespace RandomizerMod.RandomizerData
 {
     public record StartDef
     {
+        /// <summary>
+        /// The name of the start. Names should be unique.
+        /// </summary>
         public string Name { get; init; }
-
-        // respawn marker properties
+        /// <summary>
+        /// The scene of the start location in-game.
+        /// </summary>
         public string SceneName { get; init; }
+        /// <summary>
+        /// The x-coordinate of the start location in-game.
+        /// </summary>
         public float X { get; init; }
+        /// <summary>
+        /// The y-coordinate of the start location in-game.
+        /// </summary>
         public float Y { get; init; }
+        /// <summary>
+        /// The map zone of the start location in-game.
+        /// </summary>
         public MapZone Zone { get; init; }
 
-        // logic info
+        /// <summary>
+        /// The transition which is used as the initial logical progression for this start location.
+        /// </summary>
         public string Transition { get; init; }
 
-        // Primitive logic -- check SettingsPM
+        /// <summary>
+        /// Logic evaluated by the SettingsPM to determine whether the start can be selected in the menu. Must not be null.
+        /// </summary>
         public string Logic { get; init; }
+        /// <summary>
+        /// Logic evaluated by the SettingsPM to determine whether the start can be randomly selected. If null, Logic is used instead.
+        /// </summary>
+        public string RandoLogic { get; init; }
+        /// <summary>
+        /// Flag which determines whether the start is given a button in the Start Locations menu. Hidden starts can still be randomly selected.
+        /// </summary>
+        public bool ExcludeFromMenu { get; init; }
 
         public virtual bool CanBeSelected(SettingsPM pm)
         {
@@ -28,12 +53,12 @@ namespace RandomizerMod.RandomizerData
 
         public virtual bool CanBeRandomized(SettingsPM pm)
         {
-            return CanBeSelected(pm);
+            return pm.Evaluate(RandoLogic ?? Logic);
         }
 
         public virtual bool DisplayInMenu(SettingsPM pm)
         {
-            return true;
+            return !ExcludeFromMenu;
         }
 
         public virtual IEnumerable<TermValue> GetStartLocationProgression(LogicManager lm)
